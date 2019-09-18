268f2e199ee86e89cd87b2fb9a00c6819e4dc02c
vagrant <vagrant@homestead>
2019-09-18 02:46:53 +0000

    管理后台 - 订单详情

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,5 +16,8 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+    //订单列表
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    //查看订单
+    $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
 });
