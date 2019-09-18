b14315adab1f1623384f643da680a51915ba177d
vagrant <vagrant@homestead>
2019-09-18 06:42:10 +0000

    管理后台 - 拒绝退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -22,4 +22,6 @@ Route::group([
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     //发货
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
+    //处理退款
+    $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
 });
