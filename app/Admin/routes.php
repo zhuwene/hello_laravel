c41ab48efb54e69216a669b6a6aacef6dfe7549b
vagrant <vagrant@homestead>
2019-09-19 06:46:21 +0000

    管理后台 - 新增、修改、删除优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -24,6 +24,14 @@ Route::group([
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
     //处理退款
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
-    //优惠券
+    //优惠券首页
     $router->get('coupon_codes', 'CouponCodesController@index');
+    //优惠券新增
+    $router->post('coupon_codes', 'CouponCodesController@store');
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
+    //优惠券编辑
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    //删除优惠券
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 });
