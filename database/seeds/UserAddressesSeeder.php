21893aad3f0d7fc97e73ecb0a4154a8e07283800
vagrant <vagrant@homestead>
2019-10-09 07:19:27 +0000

    finish

--- database/seeds/UserAddressesSeeder.php
+++ database/seeds/UserAddressesSeeder.php
@@ -4,18 +4,12 @@ use App\Models\User;
 use App\Models\UserAddress;
 use Illuminate\Database\Seeder;
 
-
 class UserAddressesSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        User::all()->each(function (User $user){
-            factory(UserAddress::class, random_int(1,3))->create([ 'user_id' => $user->id ]);
+        User::all()->each(function (User $user) {
+            factory(UserAddress::class, random_int(1, 3))->create(['user_id' => $user->id]);
         });
     }
 }
